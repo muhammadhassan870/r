0beaec92683c8719cdf7acd0288cd2248578079e
muhammadhassan870 <muhammadhassanshahid870@gmail.com>
2021-05-23 02:15:01 +0500

    initial

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+    <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+     
+      <Text> aneeq asghar  </Text> 
+    </View>
+   
+  </View>
+
+
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
